c900947280e4f5d1ab17d841ff9b45a716fa616e
Galleta12 <svilchez010@gmail.com>
2022-04-05 23:11:17 +0100

    please

--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -148,7 +148,7 @@ amqp.connect('amqp://test:test@cloud-coursework_haproxy_1', function(error0, con
 
                     channel.consume(queue, function(msg) {
                                     console.log(" [x] Received %s", msg.content.toString());
-                                    save_list(toSend);
+                                    save_list(msg.content);
                                 }, {
                                                 noAck: true
                                             });
