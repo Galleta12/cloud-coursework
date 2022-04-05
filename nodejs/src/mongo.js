ca8650c518a3fd6fbaebacc14de9e7e97a23c886
Galleta12 <svilchez010@gmail.com>
2022-04-06 00:17:10 +0100

    please

--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -162,10 +162,14 @@ setTimeout(function(){a()},5000 );
 
 function save_list(n){
   
-// nodes.push(n);
+if (!nodes.length){
+nodes.push(n);
+}else{
+  nodes.some(ids => ids.nodeID === n["nodeID"] ?  (nodes.find(e => e.nodeID === n["nodeID"])).status += 1 : nodes.push(n));
+}
   
 
- nodes.some(ids => ids.nodeID === n["nodeID"] ?  (nodes.find(e => e.nodeID === n["nodeID"])).status += 1 : nodes.push(n));
+
 
 
  
