9c08763d1c3074dc9bbe7a7f46e2ec22f1807d7d
Galleta12 <svilchez010@gmail.com>
2022-04-07 00:39:40 +0100

    please

--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -93,7 +93,7 @@ var d = new Date();
 var text = d.getFullYear() + ":"+ d.getDate() + ":" + d.getHours()+":" + d.getMinutes();
 
 
-toSend = {"hostname": myhostname, "time": d, "nodeID": nodeID};
+toSend = {"hostname": myhostname, "time": [text,d], "nodeID": nodeID};
 
 
 
@@ -173,7 +173,7 @@ function save_list(n){
 
   
   if(nodes.some( i => i.nodeID === n["nodeID"]) && nodes.some( i => i.hostname === n["hostname"])){
-    (nodes.find(e => e.nodeID === n["nodeID"])).time = d;
+    (nodes.find(e => e.nodeID === n["nodeID"])).time = [texts,d];
  
    }
      else  {
