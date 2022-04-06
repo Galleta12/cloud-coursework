eab22b0d81f3cc4d7d3b4b5d90011f2a33c86c47
Galleta12 <svilchez010@gmail.com>
2022-04-07 00:29:58 +0100

    please

--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -178,9 +178,9 @@ function save_list(n){
    }else{
      if(!nodes.includes(n["myhostname"]) ){
 
-      if(!nodes.some( i => i.myhostname === n["myhostname"])){
+      
         nodes.push(n);
-      }
+      
     
      
     }
