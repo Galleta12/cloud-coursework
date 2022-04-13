a8c8861bff5754592cc53873614f1aa904c71804
Galleta12 <svilchez010@gmail.com>
2022-04-13 20:24:42 +0100

    why

--- nodejs/src/mongo.js
+++ nodejs/src/mongo.js
@@ -404,7 +404,7 @@ async function createContainer(){
   
   console.log("If this work I almost have everything")
   try{
-          let res_check = await axios.get(`${url}/containers/${containerName}/json`).catch(function(err){console.log(err.status)});
+          let res_check = await axios.get(`${url}/containers/${containerName}/json`).catch(function(err){console.log(err)});
           console.log("Debuggin", res_check);  
           if (res_check.status == null){
 
